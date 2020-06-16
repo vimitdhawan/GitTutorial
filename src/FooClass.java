0363c8931280fee87e7f7f429e659366031c5e6c
Dhawan, Vimit (Agoda) <Vimit.Dhawan@agoda.com>
2020-06-17 00:30:25 +0700

    Added by second

--- src/FooClass.java
+++ src/FooClass.java
@@ -1,7 +1,7 @@
 public class FooClass {
     public static void main(String[] args) {
         System.out.print("foo");
-        System.out.print("Added bt master");
+        System.out.print("Added by second");
     }
 
 }
